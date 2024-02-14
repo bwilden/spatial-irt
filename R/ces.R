ab7f8062e7befb67e23dfedf2d8545f2b135273a
bwilden <bert@bwilden.com>
2024-02-14 12:54:51 -0800

    recoding ces policy vars

---
+++ R/ces.R
@@ -0,0 +1,23 @@
+
+clean_ces <- function(policy_df,
+                      cumulative_df,
+                      policy_vars,
+                      years) {
+  out_df <- left_join(policy_df, cumulative_df,
+                      by = c("case_id", "year")) %>% 
+    filter(year %in% years,
+           state == "California") %>% 
+    mutate(abortion_20weeks = if_else(abortion_20weeks == 1, 2, 1),
+           guns_permits = if_else(guns_permits == 1, 2, 1),
+           healthcare_aca = if_else(healthcare_aca == 1, 2, 1),
+           immig_border = if_else(immig_border == 1, 2, 1),
+           immig_wall = if_else(immig_wall == 1, 2, 1),
+           trade_china = if_else(trade_china == 1, 2, 1),
+           trade_canmex_include = if_else(trade_canmex_include == 1, 2, 1)) %>% 
+    select(all_of(policy_vars))
+    
+  return(out_df)
+}
+# clean_ces(policy_df = ces_policy,
+#           cumulative_df = ces_cumulative,
+#           2017:2022)
--- _targets.R
+++ _targets.R
@@ -2,21 +2,55 @@
 library(targets)
 
 tar_option_set(
-  packages = c("tibble")
+  packages = c("dplyr",
+               "here")
 )
 
 tar_source()
 
 list(
   tar_target(
-    ces2022_file,
-    here::here("data-raw", "CCES22_Common_OUTPUT_vv_topost.csv"),
+    ces_policy_file,
+    here::here("data-raw", "cumulative_ces_policy_preferences.tab"),
     format = "file"
   ),
   tar_target(
-    ces2022,
-    readr::read_csv(ces2022_file)
+    ces_policy,
+    readr::read_delim(ces_policy_file)
+  ),
+  tar_target(
+    ces_cumulative_file,
+    here::here("data-raw", "cumulative_2006-2022.rds"),
+    format = "file"
+  ),
+  tar_target(
+    ces_cumulative,
+    readRDS(ces_cumulative_file)
+  ),
+  tar_target(
+    ces_policy_qs,
+    c("abortion_always",
+      "abortion_20weeks",
+      "enviro_carbon",
+      "enviro_renewable",
+      "enviro_airwateracts",
+      "guns_bgchecks",
+      "guns_assaultban",
+      "guns_permits",
+      "healthcare_aca",
+      "healthcare_medicare",
+      "immig_legalize",
+      "immig_border",
+      "immig_wall",
+      "trade_china",
+      "trade_canmex_include"
+    )
+  ),
+  tar_target(
+    ces,
+    clean_ces(ces_policy,
+              ces_cumulative,
+              policy_vars = ces_policy_qs,
+              years = 2017:2022)
   )
-  
-
 )
